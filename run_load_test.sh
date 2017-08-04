3483b78e9a6a29e10f90f49c5e0ff5643ecf19b7
root <root@dc1.1>
2017-08-04 09:05:20 +0200

    change script for introspector

--- run_load_test.sh
+++ run_load_test.sh
@@ -1,8 +1,8 @@
-mysqlslap -h percona_proxy_dc1 -uroot -pPassWord123 --concurrency=10 --iterations=1 \
+mysqlslap -h percona_proxy_dc1 -uroot -pPassWord123 --concurrency=80 --iterations=1 \
 --auto-generate-sql \
 --auto-generate-sql-guid-primary \
---auto-generate-sql-secondary-indexes=5 \
---auto-generate-sql-execute-number=1000 \
---auto-generate-sql-unique-query-number=50 \
---auto-generate-sql-unique-write-number=10 \
---auto-generate-sql-write-number=100
+--auto-generate-sql-secondary-indexes=20 \
+--auto-generate-sql-execute-number=10000 \
+--auto-generate-sql-unique-query-number=10 \
+--auto-generate-sql-unique-write-number=500 \
+--auto-generate-sql-write-number=5000
