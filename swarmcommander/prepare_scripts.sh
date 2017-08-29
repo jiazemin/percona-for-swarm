7d798a44baf4e6169564aedd9d3e42d0506a9e2c
root <man4j@ya.ru>
2017-08-29 13:18:10 +0000

    remove commander scripts

--- swarmcommander/prepare_scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://localhost:5555/convert -H "Content-Type: text/string" -d @create_percona_cluster.ftlh | sed $'s/\r$//' > create_percona_cluster.sh
-curl -X POST http://localhost:5555/convert -H "Content-Type: text/string" -d @remove_percona_cluster.ftlh | sed $'s/\r$//' > remove_percona_cluster.sh
-
-chmod +x create_percona_cluster.sh
-chmod +x remove_percona_cluster.sh
